50fd1c9ca6cf86b44578fc1b689210987dff23c3
Biplob_Gautam <biplobgautam.work@gmail.com>
2025-10-23 22:49:24 +0530

    Base structure for API

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+let port = 8010;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname,"/views"));
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(port , ()=>{
+    console.log("App is listening");
+});
